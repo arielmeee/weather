436e2d8d80d57753eb02fe1793f399a5b6897721
Ariel Llanita <llanitaariel@gmail.com>
2021-10-12 11:12:32 +0800

    Limit results for datalist up to 40

--- src/components/home/Input.js
+++ src/components/home/Input.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 const Input = ({ result, setQuery }) => {
   
   return (
-    <div className="form-group col-lg-3 col-md-4 col-6 mx-auto">
+    <div className="form-group col-lg-3 col-md-4 col-sm-6 col-9 mx-auto">
         <motion.input
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -17,7 +17,7 @@ const Input = ({ result, setQuery }) => {
         />
         
         <datalist id="cities">
-          {result && result.slice(0, 50).map((city) => (
+          {result && result.slice(0, 40).map((city) => (
             // "Invinsible Operator (U+2063)" is added to each option to detect 
             // if user has selected an option from the datalist to automatically query to API.
             <option value={`${city.id}\u2063`} key={city.id}>
